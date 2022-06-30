9639f84558132b7af5d86cdcc07b07592c66c71d
Adam <adam.blake@au.janus.care>
2022-06-30 14:51:10 +1000

    Update Weekday availability

--- getWeekdayAvailability/index.ts
+++ getWeekdayAvailability/index.ts
@@ -8,8 +8,9 @@ const httpTrigger: AzureFunction = async function (context: Context, req: HttpRe
         personID = +req.query.personID;
         const timeZone = req.query.timeZone || null;
         const person = new Person();
-    try {
-        let res = await person.getWeekdayAvailability(personID, timeZone);
+        try {
+        // wekday availability removed, use personal availability now 2022-06-30
+        let res = await person.getPersonalAvailability(personID, timeZone);
         context.res = {
             status: 200,
             body: res,
